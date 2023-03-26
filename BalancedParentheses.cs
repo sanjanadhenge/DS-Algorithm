13e277dbace9c60c4b3e55c51a368ef4ec3e53b1
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-26 21:45:50 +0530

    [sanjana] Add : BalancedParentheses

---
+++ BalancedParentheses.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Algorithm
+{
+    internal class BalancedParentheses
+    {
+        public void ReadFile(string filePath)
+        {
+            LinkedListStack<string> listStack = new LinkedListStack<string>();
+            string readData = File.ReadAllText(filePath);
+            string[] words = readData.Split(" ");
+            foreach (string word in words)
+            {
+              if(word.Equals("("))
+                {
+                    listStack.Push(word);
+                }
+              if(word.Equals(")"))
+                {
+                    listStack.Pop();
+                }
+            }
+            listStack.Peek();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace DS_Algorithm
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Select option \n 1.Binary Search \n 2.Insertion Sort \n 3.Bubble Sort \n 4.Merge Sort \n 5.Anagram \n 6.Prime numbers \n 7.Prime Number check \n 8.Exit");
+                Console.WriteLine("Select option \n 1.Binary Search \n 2.Insertion Sort \n 3.Bubble Sort \n 4.Merge Sort \n 5.Anagram \n 6.Prime numbers \n 7.Prime Number check \n 8.Algorithms using sorting \n 9.CustomizeMessage \n 10.linkedList Operation \n 11. Ordered List \n 12.BalancedParentheses \n 13.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
@@ -88,6 +88,10 @@ namespace DS_Algorithm
                         orderedList.ReadFile(@"C:\Users\SOURABH\Desktop\Day 3\DS-Algorithm\TextFile1.txt");
                         break;
                     case 12:
+                        BalancedParentheses balancedParentheses = new BalancedParentheses();
+                        balancedParentheses.ReadFile(@"C:\Users\SOURABH\Desktop\Day 3\DS-Algorithm\TextFile3.txt");
+                        break;
+                    case 13:
                         flag = false;
                         break;
                     
