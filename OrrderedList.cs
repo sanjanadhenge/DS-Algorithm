48e9522ad0c93551ccfc6f1cedfcbd52f1b6c5d3
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-26 21:25:13 +0530

    [sanjana] Add : Ordered List

--- OrrderedList.cs
+++ OrrderedList.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace DS_Algorithm
 {
-    internal class OrrderedList
+     internal class OrderedList
     {
+        public void ReadFile(string filepath)
+        {
+            LinkedListGeneric<int> linkedListGeneric = new LinkedListGeneric<int>();
+
+            string readData = File.ReadAllText(filepath);
+            string[] words = readData.Split(" ");
+            foreach (var word in words)
+            {
+                int item = Convert.ToInt32(word);
+                linkedListGeneric.Add(item);
+            }
+            int input = Convert.ToInt32(Console.ReadLine());
+            int result = linkedListGeneric.Search(input);
+            if (result == -1)
+            {
+                Console.WriteLine("Found");
+                linkedListGeneric.DeleteNodeAtParticularPosition(result);
+            }
+            else
+            {
+                linkedListGeneric.Add(input);
+            }
+            linkedListGeneric.Display();
+        }
     }
 }
