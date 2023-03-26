5f3a6225bc854a174bd2eb468e41d08e7579ceaa
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-26 10:50:31 +0530

    [sanjana] Add : Binary Search

---
+++ BinarySearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Algorithm
+{
+    internal class BinarySearch
+    {
+        public void ReadFile(String  filepath)
+        {
+            string readData = File.ReadAllText(filepath);
+            string[] words = readData.Split(" ");
+            Console.WriteLine("Enter the Word you want to found");
+            String search = Console.ReadLine();
+            int flag = 0;
+            foreach(var word in words)
+            {
+                if(word.Equals(search))
+                {
+                    Console.WriteLine("Found");
+                    break;
+                }
+                else
+                {
+                    flag = 1;
+                }
+          
+            }
+            if(flag == 1)
+            {
+                Console.WriteLine("The word you entered is not in the file");
+            }
+        }
+        
+    }
+
+
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Algorithm
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select option \n 1.Binary Search \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        BinarySearch binarySearch = new BinarySearch();
+                        binarySearch.ReadFile(@"C:\Users\SOURABH\Desktop\Day 3\DS-Algorithm\TextFile1.txt");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+           
+            
+        }
+    }
+
+}
